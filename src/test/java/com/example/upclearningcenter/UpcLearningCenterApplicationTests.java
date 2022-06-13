4692248af8386852607ddabb842a92547c42ed27
VivianMPQ <104032517+VivianMPQ@users.noreply.github.com>
2022-06-13 16:44:59 -0500

    feat: initial commit

---
+++ src/test/java/com/example/upclearningcenter/UpcLearningCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.upclearningcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpcLearningCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
