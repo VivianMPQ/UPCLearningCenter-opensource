adfc21fc87842f777e29842ac101a684b5a62449
VivianMPQ <104032517+VivianMPQ@users.noreply.github.com>
2022-06-13 19:52:29 -0500

    feat: added modelmapper dependency and EnhancedModelMapper

---
+++ src/main/java/com/example/upclearningcenter/shared/mapping/EnhancedModelMapper.java
@@ -0,0 +1,19 @@
+package com.example.upclearningcenter.shared.mapping;
+
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+//ir a pom y poner en dependencias modelmapper
+public class EnhancedModelMapper extends ModelMapper {
+
+    public EnhancedModelMapper() {
+        super();
+    }
+
+    public <S,T> List<T> mapList(List<S> sourceList, Class<T> targetClass){
+        return  sourceList.stream().map(item->this.map(item, targetClass)).collect(Collectors.toList());
+    }
+
+}
