30d87831c6558337856ff4e6895263fb5eef3784
VivianMPQ <104032517+VivianMPQ@users.noreply.github.com>
2022-06-17 13:15:11 -0500

    feat: added StudentService

--- src/main/java/com/example/upclearningcenter/upclearning/domain/service/StudentService.java
+++ src/main/java/com/example/upclearningcenter/upclearning/domain/service/StudentService.java
@@ -1,4 +1,28 @@
 package com.example.upclearningcenter.upclearning.domain.service;
 
+import com.example.upclearningcenter.learning.domain.model.entity.Student;
+import org.apache.coyote.Response;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+
+import java.awt.print.Pageable;
+import java.util.List;
+
+//polimorfismo
 public interface StudentService {
+
+    List<Student> getAll();
+
+    Page<Student> getAll(Pageable pageable);
+
+    Student getById(Long studentId);
+
+    Student getByName(String name);
+
+    Student create(Student student);
+
+    Student update(Long id, Student student);
+
+    ResponseEntity<?> delete (Long studentId);
+
 }
