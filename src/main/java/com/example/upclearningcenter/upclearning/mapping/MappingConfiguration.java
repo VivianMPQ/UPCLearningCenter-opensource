8dbcde3f866cdc536803f7d6a7fec024ffbe4670
VivianMPQ <104032517+VivianMPQ@users.noreply.github.com>
2022-06-29 20:42:48 -0500

    feat: added Mapping Configuration in mapping

---
+++ src/main/java/com/example/upclearningcenter/upclearning/mapping/MappingConfiguration.java
@@ -0,0 +1,23 @@
+package com.example.upclearningcenter.upclearning.mapping;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration("learningMappingConfiguration")
+public class MappingConfiguration {
+    @Bean
+    public StudentMapper studentMapper() {
+        return new StudentMapper();
+    }
+
+    @Bean
+    public SkillMapper skillMapper() {
+        return new SkillMapper();
+    }
+
+    @Bean
+    public CriterionMapper criterionMapper() {
+        return new CriterionMapper();
+    }
+
+}
